bc10e54882d1cc5da93bb6cd14dc7ec5ceedd663
JusttRelaxx <miroslav12122@gmail.com>
2014-11-03 21:16:54 +0200

    Saving animals to database.jsongit status! FINALLY! FUCK YEAHgit statusgit statusgit status! ADSKLGHDSFIH

--- animal.py
+++ animal.py
@@ -1,22 +1,42 @@
+import json
+
+animals = {}
 class Animal:
     # weight/age ratio - how much does the species grow for a month
     # till average weight (when the species gets to average weight,
-    #it stops growing)
+    # it stops growing)
+
     def __init__(
             self, species, age, name, gender,
             weight, life_expectancy, food_type, weight_age_ratio,
-            food_weight_ratio,
+            food_weight_ratio, new_born_weight,
             gestation_period=None):
 
         self.species = species
-        self.age = age
+        self.age = age;
         self.name = name
         self.gender = gender
         self.weight = weight
         self.life_expectancy = life_expectancy
         self.food_type = food_type
-        if self.gender == 'female':
-            self.gestation_period = gestation_period
+        self.gestation_period = gestation_period
         self.weight_age_ratio = weight_age_ratio
         self.food_weight_ratio = food_weight_ratio
-        #self.daily_food_consumption = food_weight_ratio * weight
+        self.new_born_weight = new_born_weight
+        self.animals = []
+        # self.daily_food_consumption = food_weight_ratio * weight
+    
+    
+    def save_animals_to_json(self):
+
+        animals[self.name] = {'species': self.species,
+                                                       'live_expectancy': self.life_expectancy,
+                                                       'food_type': self.food_type,
+                                                       'gestation_period': self.gestation_period,
+                                                       'weight_age_ratio': self.weight_age_ratio,
+                                                       'food_weight_ratio': self.food_weight_ratio,
+                                                       'new_born_weight': self.new_born_weight}
+
+        file = open('database.json', 'w+')
+        file.write(json.dumps(animals))
+        file.close()
--- animal_tests.py
+++ animal_tests.py
@@ -6,7 +6,7 @@ from animal import Animal
 class TestAnimal(unittest.TestCase):
 
     def test_animal_init(self):
-        animal = Animal("panda", 1, "Ivo", "male", 300, 6, "bamboo", 10, 0.2)
+        animal = Animal("panda", 1, "Ivo", "male", 300, 6, "bamboo", 10, 0.2, 20)
         self.assertEqual("panda", animal.species)
         self.assertEqual(1, animal.age)
         self.assertEqual("Ivo", animal.name)
@@ -16,5 +16,13 @@ class TestAnimal(unittest.TestCase):
         self.assertEqual("bamboo", animal.food_type)
         self.assertEqual(10, animal.weight_age_ratio)
         self.assertEqual(0.2, animal.food_weight_ratio)
+        self.assertEqual(20, animal.new_born_weight)
+
+    def test_animal_save(self):
+        animal1 = Animal("panda", 1, "Ivo", "male", 300, 6, "bamboo", 10, 0.2, 20)
+        animal2 = Animal("bear", 2, "Mecho", "male", 400, 3, "meat", 12, 0.4, 25)
+        animal1.save_animals_to_json()
+        animal2.save_animals_to_json()
+
 if __name__ == '__main__':
     unittest.main()
