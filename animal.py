980ede14a4c1f734ff205ceb75570829d43d20b4
JusttRelaxx <miroslav12122@gmail.com>
2014-11-05 20:12:07 +0200

    a mess.

--- animal.py
+++ animal.py
@@ -1,6 +1,8 @@
 import json
 
 animals = {}
+
+
 class Animal:
     # weight/age ratio - how much does the species grow for a month
     # till average weight (when the species gets to average weight,
@@ -8,35 +10,36 @@ class Animal:
 
     def __init__(
             self, species, age, name, gender,
-            weight, life_expectancy, food_type, weight_age_ratio,
-            food_weight_ratio, new_born_weight,
-            gestation_period=None):
+            weight):
 
         self.species = species
-        self.age = age;
+        self.age = age
         self.name = name
         self.gender = gender
         self.weight = weight
-        self.life_expectancy = life_expectancy
-        self.food_type = food_type
-        self.gestation_period = gestation_period
-        self.weight_age_ratio = weight_age_ratio
-        self.food_weight_ratio = food_weight_ratio
-        self.new_born_weight = new_born_weight
-        self.animals = []
-        # self.daily_food_consumption = food_weight_ratio * weight
-    
-    
-    def save_animals_to_json(self):
+        self.is_alive = True
+        
+        self.daily_food_consumption = food_weight_ratio * weight
+
+    """def save_animals_to_json(self):
 
         animals[self.name] = {'species': self.species,
-                                                       'live_expectancy': self.life_expectancy,
-                                                       'food_type': self.food_type,
-                                                       'gestation_period': self.gestation_period,
-                                                       'weight_age_ratio': self.weight_age_ratio,
-                                                       'food_weight_ratio': self.food_weight_ratio,
-                                                       'new_born_weight': self.new_born_weight}
+                              'live_expectancy': self.life_expectancy,
+                              'food_type': self.food_type,
+                              'gestation_period': self.gestation_period,
+                              'weight_age_ratio': self.weight_age_ratio,
+                              'food_weight_ratio': self.food_weight_ratio,
+                              'new_born_weight': self.new_born_weight}
 
         file = open('database.json', 'w+')
         file.write(json.dumps(animals))
-        file.close()
+        file.close()"""
+
+    def grow(self, age):
+        expected_weight = self.weight_age_ratio * age
+        if self.weight + expected_weight <= self.average_weight:
+            self.weight += expected_weight
+            self.age += age
+
+    def eat(self):
+        
